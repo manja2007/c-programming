c5745ad033719c67439dd1ef73a45a2e8ba72ed6
manja2007 <gmanja652@gmail.com>
2024-12-03 11:39:16 +0530

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,14 @@
+#include<stdio.>
+void main()
+{
+	int a=7, b=3, c;
+	printf("\n Arithmetic Operations");
+	printf("\n a+b=%d",c=a+b);
+	/*printf("\n a+b=%d",c=a+b);
+	printf("\n a+b=%d",c=a+b);
+	printf("\n a+b=%d",c=a+b);
+	printf("\n a+b=%d",c=a+b);*/
+	getch();
+}
+	
+	

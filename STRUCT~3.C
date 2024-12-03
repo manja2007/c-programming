e95dfbaf9ceb9f64092f01493df6a0cb8623efa9
manja2007 <gmanja652@gmail.com>
2024-12-03 11:36:17 +0530

    Add files via upload

---
+++ STRUCT~3.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main( )
+{
+	struct book
+	{
+		char name;
+		float price;
+		int pages;
+	};
+	struct book b1,b2,b3;
+	printf ( "Enter names, prices & no. of pages of 3 books\n" ) ;
+	scanf("%c%f%d",&b1.name,&b1.price,&b1.pages);
+	printf("\n book 2\n");
+	scanf("%c%f%d",&b2.name,&b2.price,&b2.pages);
+	//scanf("%c%f%d",&b3.name,&b3.price,&b3.pages);
+	printf( "And this is what you entered\n");
+	printf("%c\t%f\t%d\n",b1.name,b1.price,b1.pages);
+	printf("%c\t%f\t%d\n",b2.name,b2.price,b2.pages);
+	//printf("%c\t%f\t%d\n",b3.name,b3.price,b3.pages);
+	getch();
+	return 0 ;
+}
